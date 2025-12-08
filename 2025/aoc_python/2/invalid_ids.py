7b10384b7696df3595d6531cb15a2bcc3a181f5c
dryhen <75749022+dryhen@users.noreply.github.com>
2025-12-07 19:14:42 -0500

    invalid_ids.py - added improved processing

--- 2025/aoc_python/2/invalid_ids.py
+++ 2025/aoc_python/2/invalid_ids.py
@@ -9,28 +9,47 @@ import logging
 
 # The goal is to find the sum of all of the invalid IDs
 
+# This method is very dirty but it worksed to get the solution
+# def check_ids(id_range):
+#     invalid_ids = []
+#     # Get the ends of the range and make them integers
+#     ends = id_range.split('-')
+#     ends[0] = int(ends[0])
+#     ends[1] = int(ends[1])
+#     for id in range(ends[0], ends[1] + 1):
+#         # Make the id a string so we can use string comparison
+#         idstr = str(id)
+#         potential_rep = ""
+#         # For each digit in the ID:
+#         for dig in idstr:
+#             # Add the current digit to the potential repetition string
+#             potential_rep = "".join([potential_rep, dig])
+#             rep_twice = "".join([potential_rep, potential_rep])
+#             # See if the potential repetition twice is larger than the actual ID and stop running if so
+#             if len(rep_twice) > len(idstr):
+#                 break
+#             # If the potential repetition twice is equal to the entire id, we append the ID to our invalid IDs list
+#             if idstr == rep_twice:
+#                 invalid_ids.append(id)
+#                 break
+#     return invalid_ids
+
+# With assistance from ChatGPT, this was the superior solution
 def check_ids(id_range):
     invalid_ids = []
-    # Get the ends of the range and make them integers
-    ends = id_range.split('-')
-    ends[0] = int(ends[0])
-    ends[1] = int(ends[1])
-    for id in range(ends[0], ends[1] + 1):
-        # Make the id a string so we can use string comparison
+    start, end = map(int, id_range.split('-'))
+
+    for id in range(start, end + 1):
         idstr = str(id)
-        potential_rep = ""
-        # For each digit in the ID:
-        for dig in idstr:
-            # Add the current digit to the potential repetition string
-            potential_rep = "".join([potential_rep, dig])
-            rep_twice = "".join([potential_rep, potential_rep])
-            # See if the potential repetition twice is larger than the actual ID and stop running if so
-            if len(rep_twice) > len(idstr):
-                break
-            # If the potential repetition twice is equal to the entire id, we append the ID to our invalid IDs list
-            if idstr == rep_twice:
+        # If we aren't dealing with an even length ID we can skip it
+        if len(idstr) % 2 == 0:
+            # '//' -> integer division, Python uses float division by default
+            middle = len(idstr) // 2
+            # Slice the string to the "middle" index, then from the "middle" index through the end
+            # comparing to see if they are the same, then adding it to the list if so
+            if idstr[:middle] == idstr[middle:]:
                 invalid_ids.append(id)
-                break
+    
     return invalid_ids
 
 def main():
