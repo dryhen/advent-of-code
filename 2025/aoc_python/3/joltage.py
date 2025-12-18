e15a2c9c9da752db99917d9bbf464c4d34beb7da
dryhen <75749022+dryhen@users.noreply.github.com>
2025-12-18 14:07:04 -0500

    3.1 - fixed, 3.2 - WIP

--- 2025/aoc_python/3/joltage.py
+++ 2025/aoc_python/3/joltage.py
@@ -1,59 +1,68 @@
 import os
 import logging
 
-# Each line is a "bank" of batteries
-# Need to turn on two batteries in each bank such that the "joltage" of each battery selected (a number from 1-9)
-# Places the two greatest values together (side by side) to create a 2 digit number
-
-# Then we add these sums up
-
-# Is there an opporunity for recursion somewhere to speed this up?
-
-# Python has a built in "max" function, but I wanted to know I could still write out this logic....
-# ...that being said there has to have been a cleaner way
-# def check_bank(bank: str):
-#     highest = [0, bank[0]]
-#     second_highest = [None, None]
-#     for i in range(1, len(bank)):
-#         curr_jolt = bank[i]
-#         if curr_jolt > highest[1]:
-#             second_highest[1] = highest[1]
-#             second_highest[0] = highest[0]
-#             highest[1] = curr_jolt
-#             highest[0] = i
-#         elif second_highest[1] == None or curr_jolt > second_highest[1]:
-#             second_highest[1] = curr_jolt
-#             second_highest[0] = i
-
-#     return int(''.join([str(highest[1]), str(second_highest[1])]) if highest[0] < second_highest[0] else ''.join([str(second_highest[1]), str(highest[1])]))
-
-# Even using max() it's still flawed...
-def check_bank(bank: str):
-    highest = max(bank)
-    highest_i = bank.index(highest)
+def check_bank(bank):
+    highest_index = 0
+    for i in range(len(bank) - 1):
+        if bank[i] > bank[highest_index]:
+            highest_index = i
     
-    bank_modified = bank
-    bank_modified[highest_i] = 0
+    bank_spl = bank[highest_index + 1:]
+    second_index = 0
+    for i in range(len(bank_spl)):
+        if bank_spl[i] > bank_spl[second_index]:
+            second_index = i
 
-    second_highest = max(bank)
-    second_highest_i = bank.index(second_highest)
+    return int(''.join([str(bank[highest_index]), str(bank_spl[second_index])]))
 
-    return int(''.join([str(highest), str(second_highest)]) if highest_i < second_highest_i else ''.join([str(second_highest), str(highest)]))
+# With the help of reddit... https://www.reddit.com/r/adventofcode/comments/1pcxkif/2025_day_3_mega_tutorial/
+def check_bank_2(bank):
+    total_joltage = 0
+    batteries_remaining = 12
 
+    offset = 0
+    while batteries_remaining > 0:
+        highest = None
+        # If we start at 0, we'd want to choose a digit such that AT LEAST 11 digits are remaining on the right hand side
+        # Therefore we subtract by "batteries_remaining + 1"
+        # Once we have the first digit, we need to offset by that index so we can choose a digit after it such that AT LEAST 10
+        # digits are remaining on the right hand side
+        # (repeat until batteries_remaining is exhausted)
+        # curr_range = [offset, len(bank)-batteries_remaining+1+(12-batteries_remaining)]
+        # curr_range_vals = [bank[offset], len(bank)-batteries_remaining]
+        # upper_lim = len(bank)-batteries_remaining+1+(12-batteries_remaining) if len(bank)-batteries_remaining+1+(12-batteries_remaining) < len(bank) else len(bank)
+        for i in range(offset, len(bank)-batteries_remaining+1):
+            if highest == None:
+                highest = bank[i]
+            elif bank[i] > highest:
+                highest = bank[i]
+                offset = i + 1 # + 1 since we don't want to include that index
+                break
+        total_joltage = total_joltage + (highest * (10 ** (batteries_remaining - 1)))
+        batteries_remaining = batteries_remaining - 1
+    
+    # logging.info(total_joltage)
+    return total_joltage
 
+# Try the above with recursion...
 
 def main():
-    bank_totals = []
+    bank_totals_1 = []
+    bank_totals_2 = []
     with open(f'{os.path.dirname(os.path.abspath(__file__))}/./input.txt', 'r') as f:
         for bank in f:
             jolt_list = [int(c) for c in bank.strip()]
-            bank_totals.append(check_bank(jolt_list))
+            bank_totals_1.append(check_bank(jolt_list))
+            bank_totals_2.append(check_bank_2(jolt_list))
     # Add up all of the joltage
-    total_joltage = sum(bank_totals)
-    logging.info(f'Total joltage: {total_joltage}')
+    total_joltage = sum(bank_totals_1)
+    logging.info(f'Pt1.) Total joltage: {total_joltage}')
+
+    total_joltage_2 = sum(bank_totals_2)
+    logging.info(f'Pt2.) Total joltage: {total_joltage_2}')
     
 
 
 if __name__ == "__main__":
-    logging.getLogger().setLevel(level=logging.INFO)
+    logging.getLogger().setLevel(level=logging.DEBUG)
     main()
--- 2025/aoc_python/3/test_joltage.py
+++ 2025/aoc_python/3/test_joltage.py
@@ -1,9 +1,9 @@
 import pytest
-from joltage import check_bank
+from joltage import check_bank, check_bank_2
 
 testing_data = [
-    {"input": [7,2,2,5,8,1], "expected_result": 78},
-    {"input": [0,1,7,0,0,0], "expected_result": 17},
+    {"input": [7,2,2,5,8,1], "expected_result": 81},
+    {"input": [0,1,7,0,0,0], "expected_result": 70},
     {"input": [0,0,0,0,0,0], "expected_result": 0},
     {"input": [9,9,9,9,9,9], "expected_result": 99},
     {"input": [9,1,5,6,4,8,7,8], "expected_result": 98},
@@ -11,9 +11,23 @@ testing_data = [
     {"input": [1,1,5,8,8,5,7,1], "expected_result": 88}
 ]
 
+testing_data_2 = [
+    {"input": [7,2,2,5,8,1,2,3,6,7,4,9,0,1,2,3], "expected_result": 812367490123},
+    {"input": [7,2,8,2,5,4,1,2,3,6,7,4,9,0,1,9,2,3,3,3,4,9,2,2,2,2,1,3], "expected_result": 993349222213},
+    # {"input": [0,0,0,0,0,0], "expected_result": 0},
+    # {"input": [9,9,9,9,9,9], "expected_result": 99},
+    # {"input": [9,1,5,6,4,8,7,8], "expected_result": 98},
+    # {"input": [8,1,5,6,4,5,7,8], "expected_result": 88},
+    # {"input": [1,1,5,8,8,5,7,1], "expected_result": 88}
+]
+
 def test_check_bank():
     for data in testing_data:
         assert check_bank(data["input"]) == data["expected_result"]
+
+def test_check_bank_2():
+    for data in testing_data_2:
+        assert check_bank_2(data["input"]) == data["expected_result"]
 
 # def test_check_bank_0():
 #     assert check_bank(testing_data[0]["input"]) == testing_data[0]["expected_result"]
