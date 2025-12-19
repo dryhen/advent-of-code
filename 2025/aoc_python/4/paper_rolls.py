524b45bcd81a29097e59771e192f96a804353ef9
dryhen <75749022+dryhen@users.noreply.github.com>
2025-12-19 11:48:40 -0500

    Day 4 pt 2

--- 2025/aoc_python/4/paper_rolls.py
+++ 2025/aoc_python/4/paper_rolls.py
@@ -15,7 +15,7 @@ def find_rolls(grid):
 #    (j-1,i+1)(j,i+1)(j+1,i+1)
 # If >= 4 of the indices surrounding (j,i) are occupied, the "paper roll" cannot be moved
 def analyze_rolls(roll_coordinates, grid):
-    num_accessible = 0
+    accessible_coordinates = []
     for c in roll_coordinates:
         tl = (c[0]-1,c[1]-1)
         tm = (c[0],c[1]-1)
@@ -34,19 +34,47 @@ def analyze_rolls(roll_coordinates, grid):
                 if grid[ac[0]][ac[1]] == '@':
                     sides_surrounded = sides_surrounded + 1
         if sides_surrounded < 4:
-            num_accessible = num_accessible + 1
+            accessible_coordinates.append(c)
 
-    return num_accessible
+    return accessible_coordinates, len(accessible_coordinates)
+
+def remove_rolls(accessible_coordinates, grid):
+    modified_grid = grid
+    for ac in accessible_coordinates:
+        modified_grid[ac[0]][ac[1]] = 'X'
+
+    return modified_grid
+
+# This seems like it's slower than it should be
+def recursively_remove(grid):
+    # Find all of the rolls
+    roll_coordinates = find_rolls(grid)
+    # Find the ones we can currently remove
+    accessible_coordinates, num_accessible_rolls = analyze_rolls(roll_coordinates, grid)
+    # Remove the rolls
+    modified_grid = remove_rolls(accessible_coordinates, grid)
+    # We have no more count to add if no more are accesible, so return 0
+    if num_accessible_rolls == 0:
+        return 0
+    else:
+        # Otherwise, add the current number removed and keep going with the updated grid
+        return num_accessible_rolls + recursively_remove(modified_grid)
+
+
 
 def main():
     with open(f'{os.path.dirname(os.path.abspath(__file__))}/./input.txt', 'r') as f:
         grid = []
         for line in f:
-            grid.append(line.strip())
+            # Make our string into a list so it's mutable
+            grid.append(list(line.strip()))
         roll_coordinates = find_rolls(grid)
-        num_accessible_rolls = analyze_rolls(roll_coordinates, grid)
+        _, num_accessible_rolls = analyze_rolls(roll_coordinates, grid)
 
         logging.info(f'Number of accessible rolls: {num_accessible_rolls}')
+        # Part 2
+        total_removable = recursively_remove(grid)
+        logging.info(f'Total removable rolls: {total_removable}')
     
 
 
--- 2025/aoc_python/4/test_paper_rolls.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-testing_data = [
-    {}
-]
-
-testing_data_2 = [
-    {}
-]
